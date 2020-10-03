6130f7a683306ca5df938760f2a2325f2d82eec3
PitterPatterns <40621604+PitterPatterns@users.noreply.github.com>
2020-10-03 13:39:13 -0500

    Added textContent

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const name = document.getElementById('name');
+const other = document.querySelector('other-title');
+const colors = document.getElementById('color');
+
+name.focus();
+
+
+
+
+colors.textContent = "Please select a T-shirt theme";
--- script.js
+++
@@ -1,4 +0,0 @@
-var name = document.getElementById('name').focus();
-var other = document.querySelector('other').style.display = "none";
-
-
