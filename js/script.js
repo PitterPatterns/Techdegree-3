f3e14c7fc32bc7943cc9f3c833300b21b9936258
PitterPatterns <40621604+PitterPatterns@users.noreply.github.com>
2020-10-29 06:32:56 -0500

    added comments/hidden text input

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const name = document.getElementById('name');
-const other = document.querySelector('other-title');
+const other = document.querySelector('other');
+const otherT = document.getElementById('other-title');
 const colors = document.getElementById('color');
 
+
+
 name.focus();
+/** Name field is ready for user input */
+
+otherT.hidden = 'true';
+/** Hiding the text field initally */
 
 
+other.addEventListener('click', () => {
+  if (otherT.hidden == true) {
+    otherT.style.display = 'block';
+  } else 
+    otherT.style.display = 'none';
+});
 
+/** Giving the 'other' option the clickEventLitener */
 
-colors.textContent = "Please select a T-shirt theme";
+colors.innerHTML = "Please select a T-shirt theme";
