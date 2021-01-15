ee1f303d91413c769108ee9feee99e4ede5b02ac
PitterPatterns <40621604+PitterPatterns@users.noreply.github.com>
2021-01-14 19:18:59 -0600

    Added some querySeletors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const name = document.getElementById('name');
 const title = document.getElementById('title');
 const otherT = document.getElementById('other-title');
-const colors = document.getElementById('color');
-const other = title.querySelectorAll('option')[5];
+const colors = document.querySelector('#color');
+const design = document.getElementById('design');
+const colorop = document.querySelectorAll('#color option');
+const desiop = document.querySelectorAll('#design option');
+const sizes = document.getElementById('size');
+
 
 /** Name field is ready for user input */
 name.focus();
@@ -22,17 +26,44 @@ title.addEventListener('change', (e) => {
   }
 });
 
-
 /** Adding the "Please select a T-shirt theme" option & text */
 const oppie = document.createElement('option');
 const Tnode = document.createTextNode('Please select a T-shirt theme');
-oppie.value = 'Select Theme';
 oppie.appendChild(Tnode);
-
-console.log(oppie);
-/** Add option tag to the specific Index */
-colors.add(oppie, 0);
-oppie.selected = true;
+colors.appendChild(oppie);
 
 
+/** Puttung focus on "Please select a T-shirt theme" & hiding everything else */
+if (desiop[0].textContent == 'Select Theme') {
+  colors.hidden = true;
+  colors[6].selected = true;
+}
+/*console.log(colors);*/
+
+
+/** Changing the sizes to start at "Small" */
+sizes[0].selected = true;
+
+
+
+
+console.log(colorop);
+console.log(desiop[0].textContent);
 console.log(colors);
+
+
+
+/*design.addEventListener('change', (e) => {
+  if (e.target.value == 'js puns') {
+    otherT.style.display = 'block';
+  } else {
+    otherT.style.display = 'none';
+  }
+});*/
+
+
+
+
+
+
+
